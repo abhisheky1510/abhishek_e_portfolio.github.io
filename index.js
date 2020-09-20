f4b47b8a5118ae3bcea0d16ac8306df538919580
abhishekphalak <42695340+abhishekphalak@users.noreply.github.com>
2020-09-20 20:31:28 +0530

    aa

--- index.js
+++ index.js
@@ -1,37 +1,80 @@
-const sections = [...document.querySelectorAll('section')];
-const link = (id) => document.querySelector(`a[href="${id}"]`);
+/*!
+ * classie - class helper functions
+ * from bonzo https://github.com/ded/bonzo
+ * 
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
 
-const inView = (element) => {
-    var top = element.offsetTop;
-    var height = element.offsetHeight;
+/*jshint browser: true, strict: true, undef: true */
+/*global define: false */
 
-    while (element.offsetParent) {
-        element = element.offsetParent;
-        top += element.offsetTop;
+( function( window ) {
+
+    'use strict';
+    
+    // class helper functions from bonzo https://github.com/ded/bonzo
+    
+    function classReg( className ) {
+      return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
     }
-
-    return (
-        top < (window.pageYOffset + window.innerHeight) &&
-        (top + height) > window.pageYOffset
-    );
-};
-
-const init = () => {
-    function update() {
-        let next = false;
-
-        sections.forEach(section => {
-            const current = link(section.id);
-            if (inView(section && !next)){
-                current.classList.add('current');
-                next = true;
-            }
-            else current.classList.remove('current');
-        })
+    
+    // classList support for class management
+    // altho to be fair, the api sucks because it won't accept multiple classes at once
+    var hasClass, addClass, removeClass;
+    
+    if ( 'classList' in document.documentElement ) {
+      hasClass = function( elem, c ) {
+        return elem.classList.contains( c );
+      };
+      addClass = function( elem, c ) {
+        elem.classList.add( c );
+      };
+      removeClass = function( elem, c ) {
+        elem.classList.remove( c );
+      };
     }
-    console.log('Hle')
-    update();
-    window.addEventListener('scroll',update);
-}
-
-window.onload = () => {init()}
+    else {
+      hasClass = function( elem, c ) {
+        return classReg( c ).test( elem.className );
+      };
+      addClass = function( elem, c ) {
+        if ( !hasClass( elem, c ) ) {
+          elem.className = elem.className + ' ' + c;
+        }
+      };
+      removeClass = function( elem, c ) {
+        elem.className = elem.className.replace( classReg( c ), ' ' );
+      };
+    }
+    
+    function toggleClass( elem, c ) {
+      var fn = hasClass( elem, c ) ? removeClass : addClass;
+      fn( elem, c );
+    }
+    
+    var classie = {
+      // full names
+      hasClass: hasClass,
+      addClass: addClass,
+      removeClass: removeClass,
+      toggleClass: toggleClass,
+      // short names
+      has: hasClass,
+      add: addClass,
+      remove: removeClass,
+      toggle: toggleClass
+    };
+    
+    // transport
+    if ( typeof define === 'function' && define.amd ) {
+      // AMD
+      define( classie );
+    } else {
+      // browser global
+      window.classie = classie;
+    }
+    
+    })( window );
